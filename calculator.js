4aca3ce31fe347d4324fb16e56460558581fc92d
Nikita-vyshkevich <74305797+Nikita-vyshkevich@users.noreply.github.com>
2021-04-08 14:16:17 +0300

    added calculator

---
+++ calculator.js
@@ -0,0 +1,6 @@
+function calculate(num1, num2) {
+  return num1 + num2;
+}
+
+
+module.exports = calculate
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const calculate = require('./calculator');
 
 const server = express();
 
@@ -6,4 +7,14 @@ server.get('/', function (req, res) {
   res.send('Server is working!');
 });
 
+server.put('/', (request, response) => {
+  const num1 = parseInt(request.query.num1,10)
+  const num2 = parseInt(request.query.num2,10)
+  const result = calculate(num1, num2);
+
+  response.json(result);
+});
+
 server.listen(3000, () => console.log('Server is working!'));
+
+//добавилась функция "калькулятор"
