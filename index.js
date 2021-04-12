df85609d9cc39a81eb0954f828454af5360228b8
Nikita-vyshkevich <74305797+Nikita-vyshkevich@users.noreply.github.com>
2021-04-12 15:46:55 +0300

    added calculator

--- index.js
+++ index.js
@@ -15,30 +15,31 @@ server.put('/', (request, response) => {
   const operationType = request.query.operationType
   const num1 = request.body.num1;
   const num2 = request.body.num2;
+  let total
+  {
 
   if (operationType === "sum") {
     const result = calculator.sum(num1,num2);
-    response.json(result)
+    total = (result);
   }
   else if (operationType === "devision") {
     const result = calculator.devision(num1,num2)
-    response.json(result)
+    total = (result)
   }
   else  if (operationType === "multiplication") {
     const result = calculator.multiplication(num1,num2)
-    response.json(result)
+    total = (result)
   }
   else  if (operationType === "subtraction") {
     const result  = calculator.subtraction(num1,num2)
-    response.json(result)
+    total = (result)
   }
   else {
     console.log("operation false")
   }
 
-
-  response.json();
-});
+  response.json(total);
+}});
 
 server.listen(3000, () => console.log('Server is working!'));
 
